9b39837796b898cb16f0aded378dc01228549a70
duysterz <106291099+duysterz@users.noreply.github.com>
2023-10-17 08:12:55 -0700

    Half-way sidebar

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,7 +1,10 @@
 import './index.scss';
 
 const Layout = () => {
-    return <>Hello</>
+    return (
+    <>
+    <Sidebar />
+    </>)
     }
 
 export default Layout
---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,19 @@
+import './index,scss'
+import LogoS from '../../assets/images/Hang-Loose-Black-Circle.png'
+
+const Sidebar = () => (
+// {
+//     const [showNav, setShowNav] = useState(false);
+  
+//     return
+     
+      <div className="nav-bar">
+        <Link 
+          className="logo"
+          to="/"
+          onClick={() => setShowNav(false)}>
+          <img src={} alt="Logo" />
+          <img className="sub-logo" src={LogoSubtitle} alt="Duysterz" />
+        </Link>
+        )
+export default Sidebar
