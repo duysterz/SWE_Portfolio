73bdfea2ad391c87d37bfe0b49de8bcf7f5559f1
duysterz <106291099+duysterz@users.noreply.github.com>
2023-10-18 21:06:30 -0700

    Sidebar Styling Complete

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -12,24 +12,3 @@ const Layout = () => {
   export default Layout;
   
 
-
-
-// const Layout = () => {
-//   return (
-//     <div className="App">
-//       <Sidebar />
-//       <div className="page">
-//         <span className="tags top-tags">&lt;body&gt;</span>
-
-//         <Outlet />
-//         <span className="tags bottom-tags">
-//           &lt;/body&gt;
-//           <br />
-//           <span className="bottom-tag-html">&lt;/html&gt;</span>
-//         </span>
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default Layout
