a44758b58c4fb86b5f6cc4dc52987cc03b8af5a1
duysterz <106291099+duysterz@users.noreply.github.com>
2023-10-20 08:37:36 -0700

    About Page done- Adding Contact

---
+++ src/components/About/index.js
@@ -0,0 +1,38 @@
+import './index.scss'
+
+const About = () => {
+
+    return (
+        <>
+        <div className="container about-page">
+          <div className="text-zone">
+            <h1>About me</h1>
+            <p>
+            I'm relatively new to the field of Software Engineering,
+            but I've been actively enhancing my skills through 
+            various online bootcamps and courses. 
+            These educational experiences have provided me with a 
+            solid foundation in coding, problem-solving, and 
+            software development practices.
+          </p>
+          <p align="LEFT">
+          My interest has particularly gravitated towards Front-End 
+          Development. I enjoy the creative aspect of building user 
+          interfaces and the immediate impact it has on the user 
+          experience. The blend of design and technology in front-end
+          work excites me, and I'm keen on diving deeper into this 
+          area.
+          </p>
+          <p>
+          In addition to front-end, I've also developed a strong 
+          interest in Data Analysis. The ability to make data-driven 
+          decisions and uncover insights from raw data fascinates me. 
+          I'm eager to explore the intersection of data analysis and 
+          software development as I continue my journey in tech.
+          </p>
+          </div>
+        </div>
+      </>      
+    )
+}
+export default About 
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,26 +1,17 @@
-import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import './index.scss';
 
 const Home = () => {
-  const [letterClass, setLetterClass] = useState('text-animate');
-
-  // useEffect(() => {
-  //   return setTimeout(() => {
-  //     setLetterClass('text-animate-hover');
-  //   }, 4000);
-  // }, []);
-
   return (
     <>
       <div className="container home-page">
         <div className="text-zone">
           <h1>
-            <span className={letterClass}>Hello,</span>
+            <span>Hello,</span>
             <br />
-            <span className={`${letterClass} _13`}>I'm Duy Le</span>
+            <span>I'm Duy Le</span>
           </h1>
-          <h2> Software Developer </h2>
+          <h2>Software Developer</h2>
           <Link to="/contact" className="flat-button">
             CONTACT ME
           </Link>
